e841d91fbcd3ed06a4f1002fdfd2e962c0e7da3e
diyajose <diyajoseparayankal@gmail.com>
2023-12-05 17:24:56 +0530

    new changes

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 new TypeIt("#typingName", {
-    strings: "Diya Jose!",
-    speed: 200,
-    waitUntilVisible: true,
-    afterComplete: function (instance) {
-        setTimeout(instance.destroy, 3000)
-        
-      }
+  strings: "Diya Jose!",
+  speed: 200,
+  waitUntilVisible: true,
+  afterComplete: function (instance) {
+    setTimeout(instance.destroy, 3000)
+
+  }
 }).go();
